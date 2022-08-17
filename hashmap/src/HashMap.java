0347640ec50de607d6043fc2e8ee6422a7d4da7a
zexho <zouzhihao994@gmail.com>
2022-08-17 17:00:01 +0800

    feat: AbstractMap.remove()

--- hashmap/src/HashMap.java
+++ hashmap/src/HashMap.java
@@ -39,6 +39,32 @@ public class HashMap<K, V> extends AbstractMap<K, V> {
 
     @Override
     public void remove(K k) {
+        int hashcode = k.hashCode();
+        int index = hashcode % capacity;
+        Entry<K, V> e = entries[index];
+        while (e != null) {
+            if (e.getKey().equals(k)) {
+                Entry<K, V> pre = e.getPre();
+                Entry<K, V> next = e.getNext();
+                if (pre == null) {
+                    // 位于链表的第一个
+                    entries[index] = e.getNext();
+                } else if (next == null) {
+                    // 位于链表的最后一个
+                    pre.setNext(null);
+                } else {
+                    // 位于链表的中间位置
+                    pre.setNext(next);
+                    next.setPre(pre);
+                }
+                // 将e的pre,next指针设为null
+                e.setPre(null);
+                e.setNext(null);
+                size--;
+                return;
+            }
+            e = e.getNext();
+        }
     }
 
 
